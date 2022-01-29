5f39cf574d0bd36e10f1ec89dd4a0b3557947991
Dobromir <dobromir.pal@gmail.com>
2022-01-29 03:41:21 +0100

    file upload component and view

---
+++ src/views/Import-File.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <UploadFile />
+  </v-container>
+</template>
+
+<script>
+import UploadFile from '../components/upload-file.vue'
+export default {
+  components: {
+    UploadFile
+  }
+}
+</script>
