7c4af222961b9b210fb07b686dad9bffdf60b4df
Dobromir <dobromir.pal@gmail.com>
2022-01-30 16:13:50 +0100

    updated title and favicon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config.plugin('html')
+    .tap(args => {
+      args[0].title = 'Realtimeplot'
+      return args
+    })
+  }
 }
